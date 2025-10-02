016d8e71d5d49580c934ab88f7f0df588653aab7
Mia03 <mia03g04@hotmail.com>
2025-10-02 14:21:21 +1000

    Answer questions for each line of random print functions

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+#Amelia Wilson
+"""
+CP1404 - Practical 3
+randoms.py
+Random function testing.
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+# smallest: 5, largest: 20
+print(random.randrange(3, 10, 2))   # line 2
+# smallest: 3, largest: 9, cannot be 4
+print(random.uniform(2.5, 5.5))  # line 3
+# smallest: 2.5, largest: 5.5
+
