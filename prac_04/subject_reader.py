ddca6bb479041eb9b9d6b9a8eb9c2f150b6c49e2
Mia03 <mia03g04@hotmail.com>
2025-10-07 13:34:54 +1000

    Modify load_data() to return a nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,21 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
+    input_file = open(FILENAME)
+    data = load_data(input_file)
+    input_file.close()
     print(data)
 
 
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(filename)
+def load_data(input_file):
+    """Read data from file formatted like: subject, lecturer, number of students."""
+    subjects = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])  # Convert number of students to int
+        subjects.append(parts)
+    return subjects
 
 
 main()
