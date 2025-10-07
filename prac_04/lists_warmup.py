c9b4ca8de95c0179a883d8d1adac60c0fff2c572
Mia03 <mia03g04@hotmail.com>
2025-10-07 12:29:28 +1000

    Add the top docstring

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,3 +1,9 @@
+#Amelia Wilson
+"""
+CP1404 Pratical-4
+lists_warmup.py
+"""
+
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
 # numbers[0]
@@ -31,9 +37,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 numbers[0] = "ten"
 #2. Change the last element of numbers to 1
 numbers[-1] = 1
-
 #3. Print all the elements from numbers except the first two (slice)
 print(numbers[2:])
-
 #4. Print whether 9 is an element of numbers
 print(9 in numbers)
