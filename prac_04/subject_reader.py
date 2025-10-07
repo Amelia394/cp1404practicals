6b1a29892982c3fc640002a274607c121237e3b4
Mia03 <mia03g04@hotmail.com>
2025-10-07 13:50:19 +1000

    Format and add docstrings

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,13 +5,12 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
-
 def main():
+    """Read subject data from file and display subject details."""
     input_file = open(FILENAME)
     subject_details = load_subject_details(input_file)
     input_file.close()
     display_subject_details(subject_details)
-
 
 def load_subject_details(input_file):
     """Read data from file formatted like: subject, lecturer, number of students."""
@@ -30,4 +29,5 @@ def display_subject_details(subject_details):
         lecturer = subject[1]
         num_students = subject[2]
         print(f"{subject_code} is taught by {lecturer} and has {num_students} students")
+
 main()
