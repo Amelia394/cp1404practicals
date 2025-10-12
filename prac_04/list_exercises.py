aebeacf14e4860eb080d38bb07e1e097ac1919e1
Mia03 <mia03g04@hotmail.com>
2025-10-12 11:41:21 +1000

    Add username checker and docstring

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,10 @@
 # Amelia Wilson
 """
-
+CP1404 Practical 4
+list_exercises.py
+ask user for 5 numbers print first number, last number, smallest number, largest number and the average
+ask user for username, check if username is in given list if it is access granted otherwise access denied
 """
-
 
 def main():
     numbers = []
@@ -16,3 +18,15 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 main()
+
+usernames = [
+    'jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+    'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+    'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob'
+]
+
+username = input("Enter your username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
