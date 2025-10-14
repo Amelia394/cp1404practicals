2e4fb956cf861820b6fc889f4aec4cbb11ce8ae6
Mia03 <mia03g04@hotmail.com>
2025-10-14 14:12:23 +1000

    Refactored code to functions in emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,5 +6,27 @@ Estimated: 35 mins
 Actual: mins
 """
 
-email_to_name = {}
-email = input("Email: ").strip()
+def main():
+    """Store users' emails and names, then display them."""
+    email_to_name = {}
+    email = input("Email: ").strip()
+    while email != "":
+        name = extract_name(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+        if confirmation not in ("", "y"):
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ").strip()
+
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name(email):
+    """Extract a likely name from the email address."""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+    main()
