c8b0f13c6ef264094546322fa8f677230fab3d05
Mia03 <mia03g04@hotmail.com>
2025-10-14 11:34:53 +1000

    Change variable names to suit colour dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -5,21 +5,23 @@ Hexadecimal colour codes in a dictionary
 """
 
 CODE_TO_COLOUR = {
-    "QLD": "Queensland",
-    "NSW": "New South Wales",
-    "NT": "Northern Territory",
-    "WA": "Western Australia",
-    "SA": "South Australia",
-    "TAS": "Tasmania",
-    "VIC": "Victoria",
-    "ACT": "Australian Capital Territory"
+    "RED": "#ff0000",
+    "ORANGE": "#ff5800",
+    "YELLOW": "#ffff00",
+    "GREEN": "#00ff00",
+    "BLUE": "#0000ff",
+    "PURPLE": "#a020f0",
+    "PINK": "#ffc0cb",
+    "BLACK": "#000000",
+    "WHITE": "#ffffff",
+    "BROWN": "#a52a2a"
 }
 print(CODE_TO_COLOUR)
 
-colour_code = input("Enter hexadecimal colour code: ").upper()
-while colour_code != "":
-    if colour_code in CODE_TO_COLOUR:
-        print(f"{colour_code} is {CODE_TO_COLOUR[colour_code]}")
+colour_name = input("Enter colour name: ").upper()
+while colour_name != "":
+    if colour_name in CODE_TO_COLOUR:
+        print(f"{colour_name} is {CODE_TO_COLOUR[colour_name]}")
     else:
-        print("Invalid hexadecimal colour code")
-    colour_code = input("Enter hexadecimal colour code: ").upper()
+        print("Invalid colour name")
+    colour_name = input("Enter hexadecimal colour code: ").upper()
