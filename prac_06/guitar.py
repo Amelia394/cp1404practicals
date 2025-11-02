87e0d1ec0ddb573da92d844226641b01b072dc08
Mia03 <mia03g04@hotmail.com>
2025-11-02 20:08:10 +1000

    Add __int__ and __str__ to guitar.py and test the output in guitar_test.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,17 @@
+#Amelia Wilson
+"""
+CP1404 Practical 6
+guitar.py
+estimate: 40 min
+actual:
+"""
+
+class Guitar:
+    """Represents guitar as a class"""
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+    def __str__(self):
+        """Returns sting for the Programing Language"""
+        return f"{self.name} ({self.year}): ${self.cost}."
---
+++ prac_06/guitar_test.py
@@ -0,0 +1,13 @@
+#Amelia Wilson
+"""
+CP1404 Practical 6
+guitar_test.py
+estimate: 40 min
+actual:
+"""
+
+from prac_06.guitar import Guitar
+def main():
+    gibson = Guitar("Gibson L-5 CES",1922, "16035.40")
+    print(gibson)
+main()
