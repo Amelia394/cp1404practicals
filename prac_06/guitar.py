c6c0ba3bff7312bcbdbb76c61798be2824d0b8e7
Mia03 <mia03g04@hotmail.com>
2025-11-02 20:18:25 +1000

    Fix Guitar __str__ formatting and add default init values

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,11 +7,11 @@ actual:
 """
 
 class Guitar:
-    """Represents guitar as a class"""
-    def __init__(self, name, year, cost):
+    """Represents guitar as a class."""
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
     def __str__(self):
-        """Returns sting for the Programing Language"""
+        """Returns sting for the Guitar."""
         return f"{self.name} ({self.year}): ${self.cost}."
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -8,6 +8,6 @@ actual:
 
 from prac_06.guitar import Guitar
 def main():
-    gibson = Guitar("Gibson L-5 CES",1922, "16035.40")
+    gibson = Guitar("Gibson L-5 CES",1922, 16035.40)
     print(gibson)
 main()
