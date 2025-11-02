5b219521e966b30c0dcfc6b78c2e061e61feeeb9
Mia03 <mia03g04@hotmail.com>
2025-11-02 19:03:23 +1000

    Add ProgrammingLanguage class and __str__ and is_dynamic methods

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,24 @@
 #Amelia Wilsin
 """
 CP1404 - Practical 6
+programing_language.py
 Estimate:45min
 Actual:
 """
+
+class ProgrammingLanguage:
+    """Represents ProgramingLanguage as an object."""
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Returns sting for the Programing Language"""
+        return f"{self.name}, Typing = {self.typing}, Reflection = {self.reflection}, Year = {self.year}"
+
+    def is_dynamic(self):
+        """Set typing to dynamic."""
+        return self.typing.lower() == "Dynamic"
+
