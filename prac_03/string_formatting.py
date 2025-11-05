0ab9b9c6226552eec9efe79c28e0b5fc2e8d7d7a
Mia03 <mia03g04@hotmail.com>
2025-11-05 20:21:40 +1000

    Add code to guitar.py and guitar.csv

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -12,11 +12,11 @@ year = 1922
 cost = 16035.9
 
 # The 'old' manual way to format text with string concatenation (don't do this):
-#print("My guitar: " + name + ", first made in " + str(year))
+#print("My guitar.csv: " + name + ", first made in " + str(year))
 
 # A better way - using str.format() (don't do this unless you need to):
-#print("My guitar: {}, first made in {}".format(name, year))
-# print("My guitar: {0}, first made in {1}".format(name, year))
+#print("My guitar.csv: {}, first made in {}".format(name, year))
+# print("My guitar.csv: {0}, first made in {1}".format(name, year))
 # print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
 
 # And with f-string formatting, introduced in Python 3.6 (do this)
