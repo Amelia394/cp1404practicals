737489d1a41e687827614a966b71df86b3d4e9d0
Mia03 <mia03g04@hotmail.com>
2025-11-05 20:35:57 +1000

    Set up More Guitars project

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -7,20 +7,26 @@ actual: 34 min
 """
 
 class Guitar:
-    """Represents guitar.csv as a class."""
-    def __init__(self, name="", year=0, cost=0):
+    """Represent a Guitar object."""
+
+    def __init__(self, name, year, cost):
+        """Show guitar class in terms of name, year and cost"""
         self.name = name
         self.year = year
         self.cost = cost
+
     def __str__(self):
-        """Returns sting for the Guitar."""
-        return f"{self.name} ({self.year}): ${self.cost}."
+        """Return guitar represented as a string."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
-        """Return age of guitar.csv."""
-        current_year = 2025
-        return current_year - self.year
+        """Get age of guitar."""
+        return 2025 - self.year  # update year as needed
 
     def is_vintage(self):
-        """Return True if the guitar.csv is 50 or more years old."""
+        """Check if guitar is over 50 years old."""
         return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Define < operator for sorting by year."""
+        return self.year < other.year
