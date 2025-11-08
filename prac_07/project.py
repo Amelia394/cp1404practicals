312171e6931a9be9d45f9d06dfc6ea5f885c4f93
Mia03 <mia03g04@hotmail.com>
2025-11-08 17:08:47 +1000

    Add Project class skeleton and docstrings with time estimate

---
+++ prac_07/project.py
@@ -0,0 +1,31 @@
+#Amelia Wilson
+"""
+CP1404 practical 7
+project.py
+estimate: 40 mins
+actual:
+"""
+
+class Project:
+    """Represent project as a class."""
+
+    def __init__(self, name: str, start_date: date, priority: int,
+                 cost_estimate: float, completion: int):
+        self.name = name
+        self.start_date = start_date
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion = int(completion)
+
+    def is_complete(self) -> bool:
+        """Return True if the project is done."""
+        return self.completion == 100
+
+    def __str__(self) -> str:
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion}%")
+
+    def __lt__(self, other):
+        """Sort projects by priority level."""
+        return self.priority < other.priority
---
+++ prac_07/project_management.py
@@ -0,0 +1,8 @@
+#Amelia Wilson
+"""
+CP1404 practical 7
+project_management.py
+estimate: 40 mins
+actual:
+"""
+
