42cea894153e0879c397f8a1348b26e81edb244d
Mia03 <mia03g04@hotmail.com>
2025-11-08 17:42:19 +1000

    Add filter by date and add new project features

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -59,6 +59,32 @@ def display_projects(projects):
     for p in complete:
         print(f"  {p}")
 
+def filter_projects_by_date(projects):
+    date_string = input("Show projects that start after date (dd/mm/yyyy): ").strip()
+    try:
+        filter_date = datetime.strptime(date_string, "%d/%m/%Y").date()
+    except ValueError:
+        print("Invalid date format.")
+        return
+    filtered = [p for p in projects if p.start_date >= filter_date]
+    for p in sorted(filtered):
+        print(p)
+
+def add_new_project(projects):
+    print("Let's add a new project")
+    name = input("Name: ").strip()
+    start_date_str = input("Start date (dd/mm/yyyy): ").strip()
+    try:
+        start_date = datetime.strptime(start_date_str, "%d/%m/%Y").date()
+    except ValueError:
+        print("Invalid date format; project not added.")
+        return
+    priority = int(input("Priority: ").strip())
+    cost_estimate = float(input("Cost estimate: $").strip())
+    completion = int(input("Percent complete: ").strip())
+    projects.append(Project(name, start_date, priority, cost_estimate, completion))
+    print("Project added.")
+
 def main():
     filename = FILENAME
     projects = load_projects(filename)
@@ -79,6 +105,10 @@ def main():
         elif choice == "d":
             display_projects(projects)
             save_projects(filename, projects)
+        elif choice == "f":
+            filter_projects_by_date(projects)
+        elif choice == "a":
+            add_new_project(projects)
         else:
             print("Feature not implemented yet.")
 
