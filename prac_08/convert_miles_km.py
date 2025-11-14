6a06b7fff86f6681aa121fba73031e5d018c711d
Mia03 <mia03g04@hotmail.com>
2025-11-14 21:58:32 +1000

    Add handle_calculation and get_valid_miles to convert_miles_km.py

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,32 @@
+"""
+CP1404 Practical 8
+converting miles to km
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Amelia Wilson'
+MILES_CONVERSION = 1.60934
+
+class ConvertMilesKmApp(App):
+    def build(self):
+        """build the Kivy app from the kv file"""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+    def handle_calculate(self):
+        miles_value = self.get_valid_miles()
+        km_value = miles_value*MILES_CONVERSION
+        self.root.ids.km_label.text = str(km_value)
+
+    def get_valid_miles(self):
+        """ """
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+ConvertMilesKmApp().run()
