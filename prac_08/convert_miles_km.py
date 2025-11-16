d9eb5b5f315ca27ee2c85915922107e27ff3c0f6
Mia03 <mia03g04@hotmail.com>
2025-11-16 21:25:52 +1000

    Add docstrings to functions in convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -12,25 +12,25 @@ MILES_CONVERSION = 1.60934
 
 class ConvertMilesKmApp(App):
     def build(self):
-        """build the Kivy app from the kv file"""
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def handle_calculation(self):
-        """ """
+        """Convert miles to km."""
         miles_value = self.get_valid_miles()
         km_value = miles_value * MILES_CONVERSION
         self.root.ids.km_label.text = str(km_value)
 
     def handle_increment(self, change):
-        """ """
+        """Function for un and down increment button."""
         value = self.get_valid_miles() + change
         self.root.ids.input_number.text = str(value)
         self.handle_calculation()
 
     def get_valid_miles(self):
-        """ """
+        """Makes sure the input is a valid mile."""
         try:
             value = float(self.root.ids.input_number.text)
             return value
