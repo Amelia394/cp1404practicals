beca8f87980ec242337d994e2d511ebb5a7f1c23
Mia03 <mia03g04@hotmail.com>
2025-11-21 09:45:21 +1000

    import random for reliability test

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,6 +4,7 @@ CP1404 Practical 09 unreliable_car.py
 """
 
 from prac_09.car import Car
+import random
 
 class UnreliableCar(Car):
     """Different version of car that has a reliability value."""
@@ -15,3 +16,8 @@ class UnreliableCar(Car):
 
     def drive(self, distance):
         """Attempt to drive the car."""
+        random_number = random.uniform(0, 100)
+        if random_number < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
