4a4374850c669dcfde76f5f71dfd639b3c5e24b3
Mia03 <mia03g04@hotmail.com>
2025-11-20 15:51:00 +1000

    Restart the meter (start a new fare) and then drive the car 100 km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,5 +1,4 @@
 #Write lines of code for each of the following (hint: use the methods available in the Taxi class):
-#Print the taxi's details and the current fare
 #Restart the meter (start a new fare) and then drive the car 100 km
 #Print the details and the current fare
 
@@ -11,3 +10,6 @@ from prac_09.taxi import Taxi
 my_taxi = Taxi('Prius 1', 100, 1.23)
 Taxi.drive(my_taxi, 40)
 print(my_taxi)
+print(f"Current fare: $ {Taxi.get_fare(my_taxi)}")
+Taxi.start_fare(my_taxi)
+Taxi.drive(my_taxi, 100)
