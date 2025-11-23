28477e51c67d0ba2791176aec777cf9cd1af8a7c
Mia03 <mia03g04@hotmail.com>
2025-11-23 23:14:19 +1000

    Add docstrings to silver_service_taxi.py

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,19 +8,19 @@ from prac_09.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    """ """
+    """Taxi that charges more based on fanciness """
     flagfall = 4.50
     def __init__(self, name, fuel, fanciness):
-        """ """
+        """Initialise class for fancier taxis."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
 
     def get_fare(self):
-        """  """
+        """Retrieves the fare price including flagfall."""
         base_fare = super().get_fare()
         return base_fare + self.flagfall
 
     def __str__(self):
-        """  """
+        """Return a string representation of fare including flagfall."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
