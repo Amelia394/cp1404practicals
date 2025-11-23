58405faf944bce85c7dcfcabb81bee65ba90228d
Mia03 <mia03g04@hotmail.com>
2025-11-23 22:04:42 +1000

    Add SilverServiceTaxi Class to silver_service_taxi.py

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """ """
+
+    def __init__(self, name, fuel, fanciness):
+        """ """
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
+
