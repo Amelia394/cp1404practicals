fd93bc7fb8ea29b8ed7df0d5532e7da86bde6c33
Mia03 <mia03g04@hotmail.com>
2025-11-27 22:11:47 +1000

    complete to do

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -1,4 +1,4 @@
-#Amelia Wilson
+# Amelia Wilson
 """
 CP1404 Practical 10
 Testing code using assert and doctest
@@ -10,9 +10,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    #return s * n
     return " ".join([s] * n)
-
 def is_long_word(word, length=5):
     """
     Determine if the word is as long or longer than the length passed in
@@ -23,47 +21,33 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
-
-
+    return len(word) >= length
+def format_sentence(phrase):
+    """
+    Format a phrase as a sentence starting with a capital
+    and ending with one full stop.
+    """
+    phrase = phrase.strip()
+    if not phrase:
+        return "."
+    phrase = phrase.rstrip(".")
+    parts = phrase.split(" ", 1)
+    first_word = parts[0].capitalize()
+    if len(parts) > 1:
+        rest = parts[1]
+        return f"{first_word} {rest}."
+    else:
+        return first_word + "."
 def run_tests():
     """Run the tests on the functions."""
-    # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
-    # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
-
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
-    # assert test with custom message,
-    # used to see if Car's init method sets the odometer correctly
-    # this should pass (no output)
     car = Car()
-    assert car._odometer == 0, "Car does not set odometer correctly"
-
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
+    assert car._odometer == 0, "Car doesn't set odometer"
+    car = Car()
+    assert car.fuel == 0, "Car default fuel isn't set right"
     car = Car(fuel=10)
-
-
+    assert car.fuel == 10, "Car fuel (10) not set correctly"
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
+doctest.testmod()
